13a977695df9b617db2717a6721ab67e87b101ce
ShGuan <305666742@qq.com>
2019-05-05 14:38:58 +0800

    Initial commit

---
+++ src/main/java/com/njupt/ciot/graduate_bill/GraduateBillApplication.java
@@ -0,0 +1,13 @@
+package com.njupt.ciot.graduate_bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateBillApplication.class, args);
+	}
+
+}
